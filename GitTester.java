9b7fd7d3e59ef2bff36456816bbd8525a57706d4
Matthew Nunes <matthewanunes@gmail.com>
2012-08-08 14:13:28 +0100

    Got rid of the faulty ignore file and added a construtor to GitTester

--- GitTester.java
+++ GitTester.java
@@ -1,7 +1,12 @@
 public class GitTester
 {
+	public GitTester()
+	{
+		System.out.println("I have a constructor");
+	}
 	public static void main(String[] args)
 	{
+		GitTester gt = new GitTester();
 		System.out.println("Hello Git");
 	}
 }
