0f2b0446e0df11335c1ee559dae0abfcd278fd20
Matthew Nunes <matthewanunes@gmail.com>
2012-08-09 10:48:56 +0100

    Commit from master branch

--- GitTester.java
+++ GitTester.java
@@ -1,8 +1,10 @@
 public class GitTester
 {
+
 	public GitTester()
 	{
 		System.out.println("I have a constructor");
+		System.out.println("Lets just be a pain");
 	}
 	public static void main(String[] args)
 	{
